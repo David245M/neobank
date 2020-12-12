4798b8fe0f4c88fad0730b82b083b5b854a9bb99
Mykhailo Kokadii <m.kokadii@pmpartners.com.ua>
2020-12-13 01:30:23 +0200

    Fixed __dirname value

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import express from 'express'
-import path from 'path'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const { port } = config
 import db from './server/db.js'
 import config from './server/config.js'
-const { port } = config
-
 import signIn from './server/routes/login.js'
 import register from './server/routes/register.js'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const app = express()
 app.use(express.json())
@@ -14,9 +17,9 @@ app.post('/login', signIn)
 app.post('/register', register)
 
 if (process.env.NODE_ENV === 'production') {
-  app.use('/', express.static(path.join( 'client', 'build')))
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')))
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve( 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
 
